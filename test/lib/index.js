208df140997c9bf4515880a081ff1aed05c2e01e
Alexey Litvinov <iamhoneyboy@gmail.com>
2016-01-31 16:01:44 +0300

    post fixed

--- test/lib/index.js
+++ test/lib/index.js
@@ -7,10 +7,6 @@ const nock = require('nock');
 const httpApi = new HttpApi({retries: 0, timeout: 100});
 let scope;
 
-suiteTeardown(() => {
-  nock.restore();
-});
-
 suite('http-api', () => {
   suite('GET → success', () => {
     setup(() => {
@@ -26,18 +22,16 @@ suite('http-api', () => {
     test('should receive a response', done => {
       httpApi.get('https://api.github.com/users/sullenor')
         .then(response => {
-          assert.equal(response.login, 'sullenor');
-
           if (!scope.isDone()) {
+            console.error('pending mocks: %j', scope.pendingMocks());
             throw new Error('not all all expectations have been met');
           }
 
+          assert.equal(response.login, 'sullenor');
+
           done();
         })
-        .catch(er => {
-          console.error('pending mocks: %j', scope.pendingMocks());
-          done(er);
-        });
+        .catch(er => done(er));
     });
   });
 
@@ -57,6 +51,60 @@ suite('http-api', () => {
         .then(response => done(new Error('Successfully received a response instead of error')))
         .catch(er => {
           assert.equal(er.message, '404 Not Found ← GET https://api.github.com/users/sullenor');
+          done();
+        })
+        .catch(er => done(er));
+    });
+  });
+
+  suite('POST → success', () => {
+    setup(() => {
+      scope = nock('https://api.github.com/')
+        .post('/users/sullenor', {request: 'hello'})
+        .reply(200, require('../fixture/usersSullenor'));
+    });
+
+    teardown(() => {
+      nock.cleanAll();
+    });
+
+    test('should receive a response', done => {
+      httpApi.post('https://api.github.com/users/sullenor', {request: 'hello'})
+        .then(response => {
+          if (!scope.isDone()) {
+            console.error('pending mocks: %j', scope.pendingMocks());
+            throw new Error('not all all expectations have been met');
+          }
+
+          assert.equal(response.login, 'sullenor');
+
+          done();
+        })
+        .catch(er => done(er));
+    });
+  });
+
+  suite('POST → failure', () => {
+    setup(() => {
+      scope = nock('https://api.github.com/')
+        .post('/users/sullenor', {request: 'hello'})
+        .reply(404, 'Not found');
+    });
+
+    teardown(() => {
+      nock.cleanAll();
+    });
+
+    test('should throw an error', done => {
+      httpApi.post('https://api.github.com/users/sullenor', {request: 'hello'}, {json: false})
+        .then(response => done(new Error('Successfully received a response instead of error')))
+        .catch(er => {
+          if (!scope.isDone()) {
+            console.error('pending mocks: %j', scope.pendingMocks());
+            throw new Error('not all all expectations have been met');
+          }
+
+          assert.equal(er.message, '404 Not Found ← POST https://api.github.com/users/sullenor');
           done();
         })
         .catch(er => done(er));
