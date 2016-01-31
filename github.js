04ca1338dad0d57d0e20d473c4dd5f1b8c88da77
Alexey Litvinov <iamhoneyboy@gmail.com>
2016-01-31 22:39:38 +0300

    github api

--- github.js
+++ github.js
@@ -1,9 +1,22 @@
 'use strict';
 
+const defaults = require('lodash').defaults;
 const HttpApi = require('./lib');
 const inherits = require('util').inherits;
 
+/**
+ * @class  {HttpApi}
+ * @param  {object}  opts
+ * @param  {object}  [opts.agent]
+ * @param  {boolean} [opts.json]    true by default
+ * @param  {number}  [opts.retries] 5 by default (from got)
+ * @param  {number}  [opts.timeout]
+ * @param  {string}  [opts.uri]     'https://api.github.com/' by default
+ * @param  {string}  [opts.token]
+ * @return {httpApi}
+ */
 function Github(opts) {
+  opts = defaults(opts, {uri: 'https://api.github.com/'});
   HttpApi.call(this, opts);
 }
 
---
+++ test/github.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const assert = require('assert');
+const Github = require('../github');
+const nock = require('nock');
+
+let scope;
+
+suite('github', () => {
+  teardown(() => nock.cleanAll());
+
+  suite('uses https://api.github.com/ uri by default', () => {
+    setup(() => {
+      scope = nock('https://api.github.com/')
+        .get('/users/sullenor')
+        .reply(200, require('./fixture/usersSullenor'));
+    });
+
+    test('should pass', done => {
+      new Github({retries: 0, timeout: 100})
+        .get('/users/sullenor')
+        .then(response => {
+          if (!scope.isDone()) {
+            console.error('pending mocks: %j', scope.pendingMocks());
+            throw new Error('not all all expectations have been met');
+          }
+
+          assert.equal(response.login, 'sullenor');
+
+          done();
+        })
+        .catch(er => done(er));
+    });
+  });
+});
