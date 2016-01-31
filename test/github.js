84ae710b525d62d7c3427f10e94fdef63c65cb2f
Alexey Litvinov <iamhoneyboy@gmail.com>
2016-02-01 00:00:31 +0300

    token implementation with odd test

--- test/github.js
+++ test/github.js
@@ -32,4 +32,32 @@ suite('github', () => {
         .catch(er => done(er));
     });
   });
+
+  suite('uses oauth token', () => {
+    setup(() => {
+      scope = nock('https://api.github.com/', {
+        reqHeaders: {
+          authorization: 'token test_token',
+        },
+      })
+        .get('/users/sullenor')
+        .reply(200, require('./fixture/usersSullenor'));
+    });
+
+    test('should pass', done => {
+      new Github({retries: 0, timeout: 100, token: 'test_token'})
+        .get('/users/sullenor')
+        .then(response => {
+          if (!scope.isDone()) {
+            console.error('pending mocks: %j', scope.pendingMocks());
+            throw new Error('not all all expectations have been met');
+          }
+
+          assert.equal(response.login, 'sullenor');
+
+          done();
+        })
+        .catch(er => done(er));
+    });
+  });
 });
